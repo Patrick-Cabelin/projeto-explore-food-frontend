4e52bbcc8f3a0f1124cb3efa25c8e500a0aca884
Patrick-Cabelin <Patrickgamer0000@gmail.com>
2023-08-16 13:23:39 -0300

    signUp

--- src/assets/pages/SignUp/index.jsx
+++ src/assets/pages/SignUp/index.jsx
@@ -1,13 +1,34 @@
-import { A } from "./style"
+import { Container, DataUser } from "./style"
+
+import {Button} from '../../components/Button'
+import {ButtonText} from '../../components/ButtonText'
+import {Input} from '../../components/Input'
+import {Icons} from '../../image/Icons'
 
 function SignUp(){
+    const {Logo} = Icons()
 
     return(
-        <div>
-            <A>
-                qiuiuiuiuidhabvhjdba
-            </A>
-        </div>
+        <Container>
+            <div>
+                <Logo/>
+                <h1>Food Explore</h1>
+            </div>
+            <div>
+                    <h2>Crie sua conta</h2>
+                <DataUser>
+                    <label htmlFor="Iname">Seu nome</label>
+                    <Input type='email' id='Iemail' placeholder='Exemplo: Colette Tatou'/>
+                    <label htmlFor="Iemail">Email</label>
+                    <Input type='email' id='Iemail' placeholder='Exemplo@exemplo.com'/>
+                    <label htmlFor="Ipassword">Senha</label>
+                    <Input type='password' id='Ipassword' placeholder='No mínimo 6 caracteres'/>
+                </DataUser>
+                <Button title={'Criar conta'} orderNumber={false}/>
+
+                <ButtonText title={'Já tenho uma conta'}/>
+            </div>
+        </Container>
     )
 }
 
--- src/assets/pages/SignUp/style.js
+++ src/assets/pages/SignUp/style.js
@@ -1,8 +1,104 @@
 import styled from 'styled-components';
 
-const A = styled.h2`
-    font: ${({theme})=> theme.FONTS.ROBOTO_GIGANT_BOLD};
-    color: red;
-`
+const Container = styled.div`
+    height: 100vh;
+    width: 32rem;
 
-export {A}
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: auto;
+
+    div:first-child{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 1rem;
+        
+        h1{
+            font: ${({theme})=>theme.FONTS.ROBOTO_BIGGER_BOLD};
+            font-size: 3.7rem;
+            color: ${({theme})=> theme.COLORS.LIGHT_100};
+            width: 21.7rem;
+        }
+        
+    }
+
+    h2{
+        display: none;
+    }
+
+    form + div{
+        width: 100%;
+        margin: 3.2rem 0;
+
+        span{
+            font: ${({theme})=>theme.FONTS.POPPINS_100_MEDIUM};
+            color: ${({theme})=> theme.COLORS.LIGHT_100};
+        }
+        
+    }
+    
+    button{
+        font: ${({theme})=> theme.FONTS.POPPINS_100_MEDIUM};
+    }
+    
+    div:last-child{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+
+    @media (min-width: 769px) {
+        flex-direction: row;
+        gap: 30.7rem;
+
+        h2{
+            display: block;
+            font: ${({theme})=> theme.FONTS.POPPINS_400_MEDIUM};
+            color: ${({theme})=> theme.COLORS.LIGHT_100};
+            margin: auto;
+        }
+        form{
+            margin-top:  3.2rem ;
+
+            label:nth-child(3) + div{
+                margin-bottom: 3.2rem;
+            }
+        }
+
+        > div:last-child{
+            background:${({theme})=> theme.COLORS.DARK_700};
+            border-radius: 1.5rem;
+            padding: 6.4rem;
+        }
+    }
+    
+    `
+const DataUser = styled.form`
+    width: 32rem;
+    margin-top: 7.3rem;
+    
+    display: flex;
+    flex-direction: column;
+    
+    label{
+        font: ${({theme})=>theme.FONTS.ROBOTO_SMALL_REGULAR};
+        color: ${({theme})=> theme.COLORS.LIGHT_400};
+    }
+
+    label ~ div{
+        width: 100%;
+        height: 4.8rem;
+    }
+    
+    div:nth-child(2){
+        margin-bottom: 3.2rem;
+    }
+    
+    
+    `
+
+export { Container, DataUser}
